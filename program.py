1514d5d4b97b0ebae225088bb2ce1870de3bfc99
BJ150 <gleebjay1@gmail.com>
2024-11-20 07:46:08 +0800

    Program with view-record

--- program.py
+++ program.py
@@ -18,3 +18,8 @@ while choice.upper() != 'D' :
         clearRec()
     elif (choice.upper() == 'D'):
         print ("Thank you! ")
+def viewRec():
+ file = open(filename, 'r')
+ with open(filename, 'r') as read_file:
+ print(read_file.read())
+ read_file.close()
